0f26935882a2de4572dcdcc5988d7edea9f51762
Diego J. Silva <diego.jslaurindo@aluno.edu.es.gov.br>
2025-09-10 11:53:35 -0300

    Update index.php

--- Aula03 3trimestre/index.php
+++ Aula03 3trimestre/index.php
@@ -11,12 +11,17 @@
             $n2=2;
 
             $soma=$n1+$n2;
-            
+
 
             echo "Meu nome é " .$nome; 
             echo "<br>Meu nome é $nome";
 
             echo"<br><br>A soma de $n1 com $n2 é igual a $soma. ";
+
+            echo"<br><br>A subtração dos números $n1 e $n2 é igual a:" .($n1-$n2);
+            echo"<br><br>A multiplicação dos números $n1 e $n2 é igual a:" .($n1*$n2);
+            echo"<br><br>A divisão entre o números $n1 e $n2 é igual a:" .($n1/$n2);
+            echo"<br><br>O operador de módulo dos números $n1 e $n2 é igual a:" .($n1%$n2);
         ?>
     </body>
 </html>
